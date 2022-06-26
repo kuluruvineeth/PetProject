57708c872754fee2aed0cdd97457518890c2090a
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-06-26 17:15:26 +0530

    Added UriMatcher to ContentProvider

--- app/src/main/java/com/agririze/petproject/data/PetProvider.java
+++ app/src/main/java/com/agririze/petproject/data/PetProvider.java
@@ -2,6 +2,7 @@ package com.agririze.petproject.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
@@ -14,6 +15,18 @@ public class PetProvider extends ContentProvider {
      * Database helper object
      */
     private PetDbHelper mDbHelper;
+
+    private static final int PETS = 100;
+
+    private static final int PET_ID = 101;
+
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY,PetContract.PATH_PETS,PETS);
+
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY,PetContract.PATH_PETS + "/#",PET_ID);
+    }
 
 
     @Override
