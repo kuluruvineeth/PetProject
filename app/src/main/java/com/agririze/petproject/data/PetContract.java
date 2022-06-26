0f7f3ca9a6c743d8d91873f3f1131417f6f9dd67
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-06-26 18:07:17 +0530

    Implemented ContentProvider getType() method

--- app/src/main/java/com/agririze/petproject/data/PetContract.java
+++ app/src/main/java/com/agririze/petproject/data/PetContract.java
@@ -1,5 +1,6 @@
 package com.agririze.petproject.data;
 
+import android.content.ContentResolver;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -17,6 +18,11 @@ public final class PetContract {
 
         public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI,PATH_PETS);
 
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
+
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
         public final static String TABLE_NAME = "pets";
         public final static String _ID = BaseColumns._ID;
         public final static String COLUMN_PET_NAME = "name";
