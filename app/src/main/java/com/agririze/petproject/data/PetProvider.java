608395c2d0d9454e7300f7316f15d2e7a6937485
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-06-28 09:39:21 +0530

    modified ContentProvider so Loader refreshes data automatically

--- app/src/main/java/com/agririze/petproject/data/PetProvider.java
+++ app/src/main/java/com/agririze/petproject/data/PetProvider.java
@@ -63,6 +63,7 @@ public class PetProvider extends ContentProvider {
             default:
                 throw new IllegalArgumentException("Cannot query unknown URI " + uri);
         }
+        cursor.setNotificationUri(getContext().getContentResolver(),uri);
         return cursor;
     }
 
@@ -101,6 +102,7 @@ public class PetProvider extends ContentProvider {
             Log.e(LOG_TAG,"Fauled to insert row for " + uri);
             return null;
         }
+        getContext().getContentResolver().notifyChange(uri,null);
         return ContentUris.withAppendedId(uri,id);
     }
 
@@ -108,18 +110,26 @@ public class PetProvider extends ContentProvider {
     public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
         SQLiteDatabase database = mDbHelper.getWritableDatabase();
 
+        int rowsDeleted;
+
         final int match = sUriMatcher.match(uri);
         switch (match){
             case PETS:
-                return database.delete(PetContract.PetEntry.TABLE_NAME,selection,selectionArgs);
+                rowsDeleted =  database.delete(PetContract.PetEntry.TABLE_NAME,selection,selectionArgs);
+                break;
             case PET_ID:
 
                 selection = PetContract.PetEntry._ID + "=?";
                 selectionArgs = new String[]{ String.valueOf(ContentUris.parseId(uri))};
-                return database.delete(PetContract.PetEntry.TABLE_NAME,selection,selectionArgs);
+                rowsDeleted =  database.delete(PetContract.PetEntry.TABLE_NAME,selection,selectionArgs);
+                break;
             default:
                 throw new IllegalArgumentException("Deletion is not supported for " + uri);
         }
+        if(rowsDeleted!=0){
+            getContext().getContentResolver().notifyChange(uri,null);
+        }
+        return rowsDeleted;
     }
 
     @Override
@@ -164,6 +174,11 @@ public class PetProvider extends ContentProvider {
         }
 
         SQLiteDatabase database = mDbHelper.getWritableDatabase();
-        return database.update(PetContract.PetEntry.TABLE_NAME,values,selection,selectionArgs);
+        int rowsUpdated = database.update(PetContract.PetEntry.TABLE_NAME,values,selection,selectionArgs);
+
+        if(rowsUpdated!=0){
+            getContext().getContentResolver().notifyChange(uri,null);
+        }
+        return rowsUpdated;
     }
 }
